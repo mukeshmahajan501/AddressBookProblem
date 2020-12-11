dd6d7585e79ae24376cce4183a3b2641e2b359ce
mukesh <mukeshmahajan501@gmail.com>
2020-12-11 21:06:08 +0530

    Added:person contact with details

---
+++ src/com/company/Person.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
+    private String phoneNum;
+
+}
