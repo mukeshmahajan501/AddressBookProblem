098906cd8f372735c2dc8c75d2f313811e535715
mukesh <mukeshmahajan501@gmail.com>
2020-12-11 21:19:30 +0530

    Added:new person in addressbook

--- src/com/company/AddressBookMain.java
+++ src/com/company/AddressBookMain.java
@@ -1,8 +1,49 @@
 package com.company;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class AddressBookMain {
 
     public static void main(String[] args) {
-        System.out.println("welcome to address book Application!!! ");
+        List<Person> addressBookMain = new LinkedList<>();
+        AddressBook addressBook = new AddressBook();
+        Person person;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Address Book !");
+        boolean isContinoue=true;
+        while (isContinoue) {
+            System.out.print("\n1. Add Person." +
+                    "\n2. View Address Book." +
+                    "\n3. Exit." +
+                    "\n Enter your choice: ");
+            int choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    person = addressBook.addPerson();
+                    addressBookMain.add(person);
+                    break;
+
+                case 2:
+                    if (addressBookMain.isEmpty()) {
+                        System.out.println("Address Book is Empty.");
+                    } else {
+                        for (Person details : addressBookMain) {
+                            System.out.println(details.toString());
+                        }
+                    }
+                    break;
+
+                case 3:
+                    isContinoue=false;
+                    System.out.println("Exit.");
+                    break;
+
+                default:
+                    System.out.println("Invalid Choice.");
+                    break;
+            }
+        }
     }
 }
--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -9,4 +9,25 @@ public class Person {
     private int zip;
     private String phoneNum;
 
+    void setPerson(String firstName, String lastName, String address, String city, String state, int zip, String phoneNum) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNum = phoneNum;
+    }
+
+    @Override
+    public String toString() {
+        return "\nFirst Name: " + firstName +
+                "\nLast Name: " + lastName +
+                "\nAddress: " + address +
+                "\nCity: " + city +
+                "\nState: " + state +
+                "\nZip: " + zip +
+                "\nPhone Number: " + phoneNum;
+    }
 }
+
